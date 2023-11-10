f8e795b794792913fc394992efe03c5e8e7f908c
r Star <starlove124760@gmail.com>
2023-11-11 03:55:23 +0800

    project init

---
+++ Assets/Samples/Testing/Ghost/GhostTesting.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityHFSM;
+
+public class GhostTesting : MonoBehaviour
+{
+    private StateMachine fsm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fsm = new StateMachine();
+        fsm.AddState("A" , onEnter : s => print("A"));
+        // 進入後快速切換
+        fsm.AddState("B" , new State(onEnter : _ => print("B") , isGhostState : true));
+        fsm.AddState("C" , onEnter : _ => print("C"));
+
+        fsm.AddTransition("A" , "B");
+        fsm.AddTransition("B" , "C");
+
+        fsm.Init();    // Prints "A"
+        fsm.OnLogic(); // Prints "B" and then "C"
+    }
+}
